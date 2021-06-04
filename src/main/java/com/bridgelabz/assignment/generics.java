184a3f8b372b066677e9a78359ddd42c378f9ac8
partg952 <dankparth@gmail.com>
2021-06-04 10:35:18 +0530

    ADDED- Method takes three float and returns maximum

--- src/main/java/com/bridgelabz/assignment/generics.java
+++ src/main/java/com/bridgelabz/assignment/generics.java
@@ -8,6 +8,13 @@ public class generics {
         return max;
 
     }
+    public static float maximumFloat(Float n1,Float n2, Float n3){
+        Float max=n1;
+        max=(n2.compareTo(max)==1) ? n2:max;
+        max=(n3.compareTo(max)==1) ? n3:max;
+        return max;
+
+    }
 
     public static void main(String[] args) {
 //        maximumInt(Integer n1, Integer n2, Integer n3);
--- src/test/java/com/bridgelabz/assignment/genericsTest.java
+++ src/test/java/com/bridgelabz/assignment/genericsTest.java
@@ -4,21 +4,39 @@ import org.junit.jupiter.api.Test;
 
 public class genericsTest {
     @Test
-    public void MaxAtFirstPlace_ReturnMax(){
+    public void MaxIntAtFirstPlace_ReturnMax(){
         Integer maximum=generics.maximumInt(3,2,1);
         Assertions.assertEquals(3,maximum);
     }
 
     @Test
-    public void MaxAtSecondPlace_ReturnMax(){
+    public void MaxIntAtSecondPlace_ReturnMax(){
         Integer maximum=generics.maximumInt(2,3,1);
         Assertions.assertEquals(3,maximum);
     }
 
     @Test
-    public void MaxAtThirdPlace_ReturnMax(){
+    public void MaxIntAtThirdPlace_ReturnMax(){
         Integer maximum=generics.maximumInt(1,2,3);
         Assertions.assertEquals(3,maximum);
+    }
+
+    @Test
+    public void MaxFloatAtFirstPlace_ReturnMax(){
+        Float maximum=generics.maximumFloat(3.1f,2.1f,1.1f);
+        Assertions.assertEquals(3.1f,maximum);
+    }
+
+    @Test
+    public void MaxFloatAtSecondPlace_ReturnMax(){
+        Float maximum=generics.maximumFloat(2.1f,3.1f,1.1f);
+        Assertions.assertEquals(3.1f,maximum);
+    }
+
+    @Test
+    public void MaxFloatAtThirdPlace_ReturnMax(){
+        Float maximum=generics.maximumFloat(1.1f,2.1f,3.1f);
+        Assertions.assertEquals(3.1f,maximum);
     }
 
 
